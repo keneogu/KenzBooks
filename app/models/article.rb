d1319bb0622cf642f0d8b4b1790f9457574a5d4b
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-09-22 12:02:55 +0100

    Add the relationship models of different models

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@ class Article < ApplicationRecord
 	enum category: [:adventure, :classic, :crime, :poetry, :action, :horror]
 	has_one_attached :image
 	has_one_attached :bodypdf
+
+	belongs_to :user
+	has_many :comments
 end

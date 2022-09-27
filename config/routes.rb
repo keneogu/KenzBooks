18c241dd84bed074e8c7eda390253ccfccf15f5e
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-09-27 20:58:35 +0100

    Add stripe checkout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'checkout', to: 'checkouts#show'
+  get 'checkout/success', to: 'checkouts#success'
+  get 'members/dashboard'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'search', to: "articles#search"
-  resources :articles do
+  resources :articles do 
     resources :comments
   end
   root "articles#index"

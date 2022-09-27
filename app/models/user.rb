ea64aecbcfde383ed1325d839b110ed559cbd1d3
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-09-27 20:55:57 +0100

    Include subscription concern in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  pay_customer
+  include SubscriptionConcern
+
+  pay_customer stripe_attributes: :stripe_attributes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :articles
-  has_many :comments
+  has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end

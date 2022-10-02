cb0fba6fe0325c467ea79bb41c734c79219b05d5
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-10-02 15:20:38 +0100

    Fix routing errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   get 'checkout', to: 'checkouts#show'
   get 'checkout/success', to: 'checkouts#success'
   get 'members/dashboard'
+
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
+    session: 'users/sessions',
+    registration: 'users/registrations'
   }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'search', to: "articles#search"
   resources :articles do 

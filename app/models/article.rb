0079359f01dc6ed07d3a24e92e5f88872442263d
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-10-02 15:21:39 +0100

    Add method to validate image

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,9 +3,19 @@ class Article < ApplicationRecord
 	has_one_attached :image
 	has_one_attached :bodypdf
 
+	validate :image_type
+
 	belongs_to :user
 	has_many :comments
 
 	validates :title, :descrition, presence: true
 	scope :desc, -> { order(created_at: :desc) }
+
+	private
+
+	def image_type
+		if image.attached? == false
+			errors.add(:image, "is missing!")
+		end
+	end
 end
